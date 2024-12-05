5cbad398a743783d69a21ac137ba6740e0e5c512
Shashi Ranjan <shashi.ranjan@jublfood.com>
2024-12-05 12:45:12 +0530

    fixes coin table cell

---
+++ CryptoExplorer/Resources/Constants/ImageConstants.swift
@@ -0,0 +1,16 @@
+//
+//  ImageConstants.swift
+//  CryptoExplorer
+//
+//  Created by Shashi Ranjan on 05/12/24.
+//
+
+import Foundation
+
+struct ImageConstants {
+    static let activeCoin = "ic_active_coin"
+    static let inactiveCoin = "ic_inactive_coin"
+    static let activeToken = "ic_active_token"
+    static let inactiveToken = "ic_inactive_coin"
+    static let newTag = "ic_new"
+}
